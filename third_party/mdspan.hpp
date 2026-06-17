71547cfff0eaa917125ec1cc153ffd8948997c2e
Sacie <sacie@example.com>
2026-06-17 23:35:52 +0200

    Add Kokkos mdspan 0.6.0 headers (vendored)

---
+++ third_party/mdspan.hpp
@@ -0,0 +1,31 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//@HEADER
+
+#ifndef MDSPAN_HPP_
+#define MDSPAN_HPP_
+
+#include "include/experimental/__p0009_bits/default_accessor.hpp"
+#include "include/experimental/__p0009_bits/full_extent_t.hpp"
+#include "include/experimental/__p0009_bits/mdspan.hpp"
+#include "include/experimental/__p0009_bits/dynamic_extent.hpp"
+#include "include/experimental/__p0009_bits/extents.hpp"
+#include "include/experimental/__p0009_bits/layout_stride.hpp"
+#include "include/experimental/__p0009_bits/layout_left.hpp"
+#include "include/experimental/__p0009_bits/layout_right.hpp"
+#include "include/experimental/__p0009_bits/macros.hpp"
+#include "include/experimental/__p2630_bits/submdspan.hpp"
+
+#endif // MDSPAN_HPP_
